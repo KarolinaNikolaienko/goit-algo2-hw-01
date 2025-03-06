193202aa493c1bfb05f8b9f4ebfd5de229f07d06
Karolina Nikolaienko <mandarinkanasolntse@gmail.com>
2025-03-06 21:30:21 +0100

    task 2 + added min_k from task 2 to main

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from task_1 import min_max
+from task_2 import min_k
 
 
 def main():
+    # Task 1
+    print("Task 1")
     arr = [9, 4, 2, -7, 3, 1, 10, -11]
     print(min_max(arr))
+
+    # Task 2
+    print("Task 2")
+    arr = [18, 30, -2, 1, -10, 76, 9, -43]
+    print(min_k(arr, 3))
+    print(min_k(arr, 6))
+
+    arr = [38, 27, 43, 3, 9, 82, 10]
+    print(min_k(arr, 1))
+    print(min_k(arr, 7))
 
 
 if __name__ == "__main__":
--- task_2.py
+++ task_2.py
@@ -1,4 +1,3 @@
-
 def min_k(arr: [], k: int, l=0):
     length_of_arr = len(arr)
     if not arr:
@@ -20,13 +19,3 @@ def min_k(arr: [], k: int, l=0):
             return min_k(right, k, new_pivot_index + l + 1)
         else:
             return min_k(left, k, l)
-
-
-arr = [9, 4, 2, -7, 3, 1, 10, -11]
-print(min_k(arr, 3))
-print(min_k(arr, 6))
-
-
-arr = [38, 27, 43, 3, 9, 82, 10]
-print(min_k(arr, 1))
-print(min_k(arr, 7))
